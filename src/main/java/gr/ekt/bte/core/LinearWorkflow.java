25c96d822342c2af72f8c3ece4e2528fcd502dfd
Panagiotis Koutsourakis <kutsurak@ekt.gr>
2013-03-04 16:00:02 +0200

    Adding a workflow interface.

---
+++ src/main/java/gr/ekt/bte/core/LinearWorkflow.java
@@ -0,0 +1,31 @@
+package gr.ekt.bte.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LinearWorkflow implements Workflow {
+    private List<ProcessingStep> process_;
+
+    public LinearWorkflow() {
+        process_ = new ArrayList<ProcessingStep>();
+    }
+
+    @Override
+    public void addStep(ProcessingStep step) {
+        process_.add(step);
+    }
+
+    @Override
+    public RecordSet run(RecordSet records) {
+        RecordSet recs = records;
+        for (ProcessingStep step : process_) {
+            recs = step.execute(recs);
+        }
+
+        return recs;
+    }
+
+    public void setProcess(List<ProcessingStep> process) {
+        process_ = process;
+    }
+}
