14ab9c8b0e445d4e84c2df16556b88e9807d3340
Panagiotis Koutsourakis <kutsurak@ekt.gr>
2013-07-30 17:13:14 +0300

    Added more comments

--- src/main/java/gr/ekt/bte/core/AbstractFilter.java
+++ src/main/java/gr/ekt/bte/core/AbstractFilter.java
@@ -1,12 +1,45 @@
 package gr.ekt.bte.core;
 
+/**
+ * Abstract class providing the interface for record filtering.
+ * <br/>
+ * <br/>
+ * The user should extend this class and override the method {@link
+ * AbstractFilter#isIncluded} in order to implement the criterion by
+ * which the records should be filtered.
+ *
+ *  @author Panagiotis Koutsourakis
+ *  @author Konstantinos Stamatis
+ *  @author Nikos Houssos
+ */
 public abstract class AbstractFilter implements ProcessingStep {
+    /**
+     * The name of the filter. Can be used for bookeping purposes.
+     */
     private String name;
 
+    /**
+     * Sets the filter's name.
+     *
+     * @param name The name of the filter
+     */
     public AbstractFilter(String name) {
         this.name = name;
     }
 
+    /**
+     * Executes this {@link ProcessingStep}.
+     * <br/>
+     * <br/>
+     * For each {@link Record} in the argument, if {@link AbstractFilter#isIncluded} returns
+     * <code>true</code> the Record is included in the return result,
+     * otherwise it is ignored.
+     *
+     * @see ProcessingStep#execute
+     *
+     * @param recs The initial set of records
+     * @return     The resulting set after the filter is applied
+     */
     @Override
     public RecordSet execute(RecordSet recs) {
         RecordSet ret = new RecordSet();
@@ -19,10 +52,23 @@ public abstract class AbstractFilter implements ProcessingStep {
         return ret;
     }
 
+    /**
+     * Gets this filter's used defined name.
+     *
+     * @return The name of the filter
+     */
     @Override
     public String getName() {
         return name;
     }
 
+    /**
+     * Abstract method that examines if the specified record should be
+     * included in the resulting set. This method is the one that
+     * should be overriden by the user that creates her own filter.
+     *
+     * @param rec The record to be examined
+     * @return    <code>true</code> if the record should be included
+     */
     public abstract boolean isIncluded(Record rec);
 }
--- src/main/java/gr/ekt/bte/core/MutableRecord.java
+++ src/main/java/gr/ekt/bte/core/MutableRecord.java
@@ -3,71 +3,71 @@ package gr.ekt.bte.core;
 import java.util.List;
 
 /**
-   An interface providing the operations for changing the data of a
-   record.
-
-   @author Panagiotis Koutsourakis
-   @author Konstantinos Stamatis
-   @author Nikos Houssos
-*/
+ *  An interface providing the operations for changing the data of a
+ *  record.
+ *
+ *  @author Panagiotis Koutsourakis
+ *  @author Konstantinos Stamatis
+ *  @author Nikos Houssos
+ */
 public interface MutableRecord extends Record {
     /**
-       Adds a new field to the record.
-
-       @param field  The name of the field
-       @param values The values to be inserted
-       @return       <code>true</code> if the values have been
-       successfully inserted
-    */
+     * Adds a new field to the record.
+     *
+     * @param field  The name of the field
+     * @param values The values to be inserted
+     * @return       <code>true</code> if the values have been
+     * successfully inserted
+     */
     public boolean addField(String field, List<Value> values);
 
     /**
-       Adds a new value to the specified field.
-
-       @param field The name of the field
-       @param value The value to be inserted
-       @return      <code>true</code> if the value has been
-       successfully inserted
-    */
+     * Adds a new value to the specified field.
+     *
+     * @param field The name of the field
+     * @param value The value to be inserted
+     * @return      <code>true</code> if the value has been
+     * successfully inserted
+     */
     public boolean addValue(String field, Value value);
 
     /**
-       Removes a field from the record.
-
-       @param field The name of the field to be removed
-       @return      <code>true</code> if the field has been
-       successfully removed.
-    */
+     * Removes a field from the record.
+     *
+     * @param field The name of the field to be removed
+     * @return      <code>true</code> if the field has been
+     * successfully removed.
+     */
     public boolean removeField(String field);
 
     /**
-       Erases a value from the specified field.
-
-       @param field The name of the field
-       @param value The value to be erased.
-       @return      <code>true</code> if the value is successfully erased.
-    */
+     * Erases a value from the specified field.
+     *
+     * @param field The name of the field
+     * @param value The value to be erased.
+     * @return      <code>true</code> if the value is successfully erased.
+     */
     public boolean removeValue(String field, Value value);
 
     /**
-       Changes the values associated with a specified field.
-
-       @param field  The name of the field to be changed
-       @param values The new set of values to be associated with the
-       field
-       @return       <code>true</code> if the field has been
-       successfully updated
-    */
+     *  Changes the values associated with a specified field.
+     *
+     * @param field  The name of the field to be changed
+     * @param values The new set of values to be associated with the
+     * field
+     * @return       <code>true</code> if the field has been
+     * successfully updated
+     */
     public boolean updateField(String field, List<Value> values);
 
     /**
-       Changes a single value in a field.
-
-       @param field     The name of the field to be changed
-       @param old_value The previous value
-       @param new_value The new value
-       @return          <code>true</code> if the value has been
-       changed successfully
+     * Changes a single value in a field.
+     *
+     * @param field     The name of the field to be changed
+     * @param old_value The previous value
+     * @param new_value The new value
+     * @return          <code>true</code> if the value has been
+     * changed successfully
      */
     public boolean updateValue(String field, Value old_value, Value new_value);
 }
