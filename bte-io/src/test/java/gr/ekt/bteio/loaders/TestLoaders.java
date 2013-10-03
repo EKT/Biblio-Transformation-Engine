4abc0f4d49ff7d3307d4afc57c7be816ce4f7847
Panagiotis Koutsourakis <kutsurak@ekt.gr>
2013-10-03 11:55:49 +0300

    Added a test for the BibTeXDataLoader

--- bte-io/src/test/java/gr/ekt/bteio/loaders/TestLoaders.java
+++ bte-io/src/test/java/gr/ekt/bteio/loaders/TestLoaders.java
@@ -42,7 +42,9 @@ import gr.ekt.bte.core.RecordSet;
 import gr.ekt.bte.exceptions.MalformedSourceException;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Test;
 
@@ -68,6 +70,64 @@ public class TestLoaders {
                 assert(rec.hasField(fl));
             }
         } catch(MalformedSourceException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testBibTeXLoader() {
+        Map<String, String> fields = new HashMap<String, String>();
+
+        fields.put("abstract", "abstract");
+        fields.put("acknowledgement", "acknowledgement");
+        fields.put("address", "address");
+        fields.put("adviser", "adviser");
+        fields.put("affiliation", "affiliation");
+        fields.put("alttitle", "alttitle");
+        fields.put("annote", "annote");
+        fields.put("author", "author");
+        fields.put("bibdate", "bibdate");
+        fields.put("bibsource", "bibsource");
+        fields.put("classcodes", "classcodes");
+        fields.put("classification", "classification");
+        fields.put("CODEN", "CODEN");
+        fields.put("confdate", "confdate");
+        fields.put("conflocation", "conflocation");
+        fields.put("conftitle", "conftitle");
+        fields.put("corpsource", "corpsource");
+        fields.put("day", "day");
+        fields.put("dimensions", "dimensions");
+        fields.put("fjournal", "fjournal");
+        fields.put("howpublished", "howpublished");
+        fields.put("ISBN", "ISBN");
+        fields.put("ISBN-13", "ISBN-13");
+        fields.put("ISSN", "ISSN");
+        fields.put("ISSN-L", "ISSN-L");
+        fields.put("journal", "journal");
+        fields.put("keywords", "keywords");
+        fields.put("LCCN", "LCCN");
+        fields.put("month", "month");
+        fields.put("note", "note");
+        fields.put("number", "number");
+        fields.put("pages", "pages");
+        fields.put("paperback", "paperback");
+        fields.put("price", "price");
+        fields.put("pubcountry", "pubcountry");
+        fields.put("publisher", "publisher");
+        fields.put("school", "school");
+        fields.put("thesaurus", "thesaurus");
+        fields.put("title", "title");
+        fields.put("treatment", "treatment");
+        fields.put("type", "type");
+        fields.put("URL", "URL");
+        fields.put("volume", "volume");
+        fields.put("year", "year");
+
+        try {
+            BibTeXDataLoader ld = new BibTeXDataLoader("src/test/resources/java.bib", fields);
+            RecordSet recs = ld.getRecords();
+            assertEquals(100, recs.size());
+        } catch (MalformedSourceException e) {
             fail(e.getMessage());
         }
     }
