fc1f6a8fa5dab4b882fd7f427b933fa2a0deb2cd
Panagiotis Koutsourakis <kutsurak@ekt.gr>
2013-12-03 12:05:32 +0200

    Add a ScreenOutputGenerator
    The new generator prints the records on screen using the
    System.out.println method.

---
+++ bte-io/src/main/java/gr/ekt/bteio/generators/ScreenOutputGenerator.java
@@ -0,0 +1,35 @@
+package gr.ekt.bteio.generators;
+
+import gr.ekt.bte.core.DataOutputSpec;
+import gr.ekt.bte.core.OutputGenerator;
+import gr.ekt.bte.core.Record;
+import gr.ekt.bte.core.RecordSet;
+import gr.ekt.bte.core.Value;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ScreenOutputGenerator implements OutputGenerator {
+    @Override
+    public List<String> generateOutput(RecordSet recs) {
+
+        for (Record rec : recs) {
+            System.out.println("==============Record==============");
+            for (String str : rec.getFields()) {
+                List<Value> vals = rec.getValues(str);
+                if (vals != null) {
+                    System.out.println(str + ": ");
+                    for (Value val : vals) {
+                        System.out.println("----: " + val.getAsString());
+                    }
+                }
+            }
+        }
+        return new ArrayList<String>();
+    }
+
+    @Override
+    public List<String> generateOutput(RecordSet recs, DataOutputSpec spec) {
+        return generateOutput(recs);
+    }
+}
